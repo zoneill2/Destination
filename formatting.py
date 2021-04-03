1b5791bd99395ab92573492334560c940fe4d59f
zoneill2 <60304687+zoneill2@users.noreply.github.com>
2021-04-03 15:38:58 -0400

    Update formatting.py

--- formatting.py
+++ formatting.py
@@ -1,7 +1,7 @@
 msg_template = """Hello {name},
 Thank you for joining {website}. We are very
 happy to have you with us.
-""" # .format(name="Justin", website='cfe.sh')
+""" 
 
 def format_msg(my_name="Zach", my_website="google.com"):
     my_msg = msg_template.format(name=my_name, website=my_website)
