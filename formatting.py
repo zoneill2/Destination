a81c294c1cf534fe774a06977835ffc31ed4ec4b
zoneill2 <60304687+zoneill2@users.noreply.github.com>
2021-04-03 16:06:34 -0400

    added email txt and way to reference the text in it

--- formatting.py
+++ formatting.py
@@ -1,8 +1,14 @@
-msg_template = """Hello {name},
-Thank you for joining {website}. We are very
-happy to have you with us.
-""" 
+import os
+
+base = os.path.dirname(os.path.abspath(__file__))
+proj_dir = os.path.dirname(base)
+msg_template = os.path.join(base,"email.txt")
+
+content = ""
+
+with open(msg_template,'r') as f:
+    content=f.read()
 
 def format_msg(my_name="Zach", my_website="google.com"):
-    my_msg = msg_template.format(name=my_name, website=my_website)
+    my_msg = content.format(name=my_name, website=my_website)
     return my_msg
